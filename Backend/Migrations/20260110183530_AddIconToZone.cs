5618efbdf88103e4cb2b65c01f7e73da8552a6c5
alibusiness <ali87mohammed@hotmail.com>
2026-01-10 21:37:53 +0300

    Add Icon field to Zone entity
    Introduces a new migration to add an 'Icon' column to the Zone model. Updates the Entity Framework model snapshot to reflect this schema change.

---
+++ Backend/Migrations/20260110183530_AddIconToZone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIconToZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "Zones",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Zones");
+        }
+    }
+}
