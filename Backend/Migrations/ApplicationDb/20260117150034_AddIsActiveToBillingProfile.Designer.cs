f51e0d08dddd12bee1f169ee0e455fb43072df6e
Ali Al-Estrabadee <ali87mohammed@hotmail.com>
2026-01-17 18:01:31 +0300

    Add IsActive field to BillingProfile model
    Introduced new EF Core migrations to add an IsActive column to the BillingProfile entity. Updated the ApplicationDbContextModelSnapshot to reflect this schema change.

--- Backend/Migrations/ApplicationDb/20260117150034_AddIsActiveToBillingProfile.Designer.cs
+++ Backend/Migrations/ApplicationDb/20260117150034_AddIsActiveToBillingProfile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260117150034_AddIsActiveToBillingProfile")]
+    partial class AddIsActiveToBillingProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -234,9 +237,6 @@ namespace Backend.Migrations.ApplicationDb
 
                     b.Property<string>("Description")
                         .HasColumnType("text");
-
-                    b.Property<bool>("IsActive")
-                        .HasColumnType("boolean");
 
                     b.Property<bool>("IsDeleted")
                         .HasColumnType("boolean");
