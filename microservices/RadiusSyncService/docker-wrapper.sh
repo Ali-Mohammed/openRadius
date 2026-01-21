c18ff9d0757744b798da66fdc15761ca63f402ad
Ali Al-Estrabadee <ali87mohammed@hotmail.com>
2026-01-21 09:17:28 +0300

    Remove unused Docker wrapper script
    Deleted docker-wrapper.sh from RadiusSyncService as it is no longer needed for Docker command execution or .NET process spawning on macOS.

--- microservices/RadiusSyncService/docker-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Wrapper script to execute Docker commands
-# This avoids issues with .NET Process spawning on macOS
-
-# Close stdin to prevent Docker from waiting for input
-exec </dev/null
-
-# Execute Docker with all arguments
-exec /Applications/Docker.app/Contents/Resources/bin/docker "$@"
