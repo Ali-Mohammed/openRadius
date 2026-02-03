d4dfb5e0f5832826812d66be4c3dfe20b3831425
Ali Al-Estrabadee <ali87mohammed@hotmail.com>
2026-02-03 22:11:01 +0300

    Add more options to the integration

---
+++ Backend/Migrations/20260203191040_AddCheckCardAvailabilityBeforeActivate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheckCardAvailabilityBeforeActivate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CheckCardAvailabilityBeforeActivate",
+                table: "SasRadiusIntegrations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckCardAvailabilityBeforeActivate",
+                table: "SasRadiusIntegrations");
+        }
+    }
+}
