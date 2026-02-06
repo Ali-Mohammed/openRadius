6cc0ac6d65e249dbb269d85932daa841b7116632
Ali Al-Estrabadee <ali87mohammed@hotmail.com>
2026-02-06 10:09:09 +0300

    Clean the database

--- Backend/update_sync_interval_default.sql
+++
@@ -1,4 +0,0 @@
--- Update existing SasRadiusIntegrations that have 0 interval to use default of 5 minutes
-UPDATE "SasRadiusIntegrations"
-SET "SyncOnlineUsersIntervalMinutes" = 5
-WHERE "SyncOnlineUsersIntervalMinutes" = 0;
